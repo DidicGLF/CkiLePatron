8fb4c337c0edad074f2bc4e4b91c067472ef2a67
Didic <didic@ik.me>
2026-06-14 14:18:13 +0200

    add snap and fix grid color

--- lancer.sh
+++ lancer.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-export LD_LIBRARY_PATH="/nix/store/ybp235ps7m4yd85v0pgvqkhd4xmxf6jq-gcc-14.3.0-lib/lib"
+export LD_LIBRARY_PATH="/nix/store/ba1mvpjflnmy15qhb3jpjzjgdyvq51yp-gcc-14.3.0-lib/lib"
 exec /home/didic/.venv-ckilepatron/bin/python "$(dirname "$0")/app.py" "$@"
